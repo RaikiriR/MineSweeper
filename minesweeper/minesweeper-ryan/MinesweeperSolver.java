b146d25f244d14dd98da5c6ed50ac2876239ff72
Ryan Bucherl <47131610+RyanBucherl@users.noreply.github.com>
2020-01-13 21:48:52 -0800

    Solution Fixed
    Got my solution to work. Still need to fix for code standards.

--- minesweeper/minesweeper-ryan/MinesweeperSolver.java
+++ minesweeper/minesweeper-ryan/MinesweeperSolver.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
 public class MinesweeperSolver{
-   private static int numCols, numRows, curBoard;
+   private static int rows, cols, curBoard;
    
-   public static void main(String[] args){
+   public static void main(String[] args)
+   {
       Scanner kb = new Scanner(System.in);
       boolean hasData = true;
       curBoard = 0;
       
-      do{
+      do
+      {
          hasData = nextBoard(kb);
       }while(hasData && kb.hasNextLine());
    }
 
-   private static boolean nextBoard(Scanner kb){
-      numRows = kb.nextInt();
-      numCols = kb.nextInt();
+   private static boolean nextBoard(Scanner kb)
+   {
+      rows = kb.nextInt();
+      cols = kb.nextInt();
       curBoard++;
-      if(numRows == 0 || numCols == 0)
+      if(cols == 0 || rows == 0)
          return false;
       
-      char[][] board = new char[numRows + 2][numCols + 2];
-      System.out.println(board.length + " " + board[0].length);
+      char[][] board = new char[rows + 2][cols + 2];
       populateBoard(kb, board);
       solveBoard(board);
       printBoard(board);
@@ -29,33 +31,39 @@ public class MinesweeperSolver{
       return true;
    }
    
-   private static void populateBoard(Scanner kb, char[][] board){
+   private static void populateBoard(Scanner kb, char[][] board)
+   {
       populateBoardEdge(board);
       String str;
       
-      for(int curRow = 1; curRow <= numRows; curRow++){
-         str = kb.next();
-         for( int curCol = 1; curCol <= numCols; curCol++){
-            board[curCol][curRow] = str.charAt(curCol - 1);
-         }
+      for(int curRow = 1; curRow <= rows; curRow++)
+      {
+    	  str = kb.next();
+    	  for(int curCol = 1; curCol <= cols; curCol++)
+    	  {
+    		  board[curRow][curCol] = str.charAt(curCol -1);
+    	  }
       }
    }
    
    private static void populateBoardEdge(char[][] board){
-      for(int curCol = 0; curCol < numCols; curCol++){
-         System.out.println(curCol + " " + numCols);
-         board[curCol][0] = '.';
-         board[curCol][numCols + 1] = '.';
-      }
-      for(int curRow = 0; curRow < numRows; curRow++){
-         board[0][curRow] = '.';
-         board[numRows + 1][curRow] = '.';
-      }
+	   for(int curRow = 0; curRow < rows+2; curRow++)
+	   {
+	         board[curRow][0] = '.';
+	         board[curRow][cols + 1] = '.';
+	   }
+	   for(int curCol = 0; curCol < cols+2; curCol++)
+	   {
+	         board[0][curCol] = '.';
+	         board[rows + 1][curCol] = '.';
+	   }
    }
    
    private static void solveBoard(char[][] board){
-      for(int y = 1; y <= numRows; y++){
-         for(int x = 1; x <= numCols; x++){
+      for(int y = 1; y <= cols; y++)
+      {
+         for(int x = 1; x <= rows; x++)
+         {
             if(board[x][y] == '.')
             {
                int mines = checkForMines(x, y, board);
@@ -67,21 +75,29 @@ public class MinesweeperSolver{
    
    private static int checkForMines(int x, int y, char[][]board){
       int mines = 0;
-      
+      if(board[x-1][y-1] == '*') mines++;
+      if(board[x-1][y] == '*') mines++;
+      if(board[x-1][y+1] == '*') mines++;
+      if(board[x][y-1] == '*') mines++;
+      if(board[x][y+1] == '*') mines++;
+      if(board[x+1][y-1] == '*') mines++;
+      if(board[x+1][y] == '*') mines++;
+      if(board[x+1][y+1] == '*') mines++;
       
       return mines;
    }
    
    private static void printBoard(char[][] board){
       System.out.println("Field #" + curBoard + ":");
-      for(int y = 1; y <= numRows; y++)
+      for(int curRow = 1; curRow <= rows; curRow++)
       {
-         for(int x = 1; x <= numCols; x++)
-         {
-            System.out.print(board[x][y]);
-         }
-         System.out.println();
+    	  for(int curCol = 1 ; curCol <= cols; curCol++)
+    	  {
+    		  System.out.print(board[curRow][curCol]);
+    	  }
+    	  System.out.println();
       }
       System.out.println();
+	  
    }
 }
